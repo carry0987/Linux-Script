09281e858d528edd8972daa0dc95324b63790a62
carry0987 <carry0987@users.noreply.github.com>
2021-03-18 00:43:31 +0800

    Update gcp-pkg.sh

--- GCP/gcp-pkg.sh
+++ GCP/gcp-pkg.sh
@@ -2,8 +2,8 @@
 
 set -e
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install zip unzip wget vim screen haveged htop
+sudo apt-get -yq dist-upgrade
+sudo apt-get -yq install zip unzip wget vim screen haveged htop
 sudo apt-get clean
 #Start haveged
 sudo systemctl enable haveged
