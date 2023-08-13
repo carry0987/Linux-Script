d5edae8efeeb2cfba787bf9bb181862c1ef2ae90
carry0987 <carry0987@users.noreply.github.com>
2023-08-14 05:42:49 +0800

    Update Install-Main-Pkg.sh

--- book_source/Setup/Install-Main-Pkg.sh
+++ book_source/Setup/Install-Main-Pkg.sh
@@ -3,8 +3,11 @@
 set -e
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install zip unzip wget vim screen haveged htop
+sudo apt-get -y install ssh zip unzip wget vim screen haveged htop ufw
 sudo apt-get clean
+# Start SSH
+sudo systemctl enable ssh --now
+sudo systemctl start ssh
 #Start haveged
 sudo systemctl enable haveged
 sudo systemctl start haveged
