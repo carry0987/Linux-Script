986a8f6e671515c8680a66d7aaf41a8d382e1f34
carry0987 <carry0987@users.noreply.github.com>
2024-02-19 20:29:04 +0800

    Update Set-LC.sh

--- book_source/Setup/Set-LC.sh
+++ book_source/Setup/Set-LC.sh
@@ -9,21 +9,21 @@ sudo timedatectl set-ntp yes
 check_user=$EUID
 user_name=$USER
 if [ $check_user -eq 0 ]; then
-    read -e -p 'Where is your .bashrc file? /home/>' select_user
-    sed -i '/HISTCONTROL/s/.*/HISTCONTROL=ignoreboth:erasedups/' /home/${select_user}/.bashrc
-    source /home/${select_user}/.bashrc
+    read -e -p -r 'Where is your .bashrc file? /home/>' select_user
+    sed -i '/HISTCONTROL/s/.*/HISTCONTROL=ignoreboth:erasedups/' "/home/${select_user}/.bashrc"
+    source "/home/${select_user}/.bashrc"
 else
-    sed -i '/HISTCONTROL/s/.*/HISTCONTROL=ignoreboth:erasedups/' /home/${select_user}/.bashrc
-    source /home/${user_name}/.bashrc
+    sed -i '/HISTCONTROL/s/.*/HISTCONTROL=ignoreboth:erasedups/' "/home/${user_name}/.bashrc"
+    source "/home/${user_name}/.bashrc"
 fi
 
 #Reboot
 reboot_os() {
     echo
-    read -p "Do you want to restart system? [y/n]>" is_reboot
+    read -p -r "Do you want to restart system? [y/n]>" is_reboot
     if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
         secs=$((5))
-        while [ $secs -gt 0 ]
+        while [[ $secs -gt 0 ]]
         do
             echo -ne 'Wait '"$secs\033[0K"' seconds to reboot'"\r"
             sleep 1
@@ -39,12 +39,11 @@ reboot_os() {
 #Set up locale
 language='C.UTF-8'
 sudo rm /etc/default/locale
-sudo touch /etc/default/locale
+echo "LANG=$language
+LANGUAGE=$language
+LC_ALL=$language
+LC_CTYPE=$language" | sudo tee /etc/default/locale > /dev/null
 sudo chmod 644 /etc/default/locale
-sudo echo 'LANG='$language >> /etc/default/locale
-sudo echo 'LANGUAGE='$language >> /etc/default/locale
-sudo echo 'LC_ALL='$language >> /etc/default/locale
-sudo echo 'LC_CTYPE='$language >> /etc/default/locale
 export LANG="${language}"
 export LANGUAGE="${language}"
 export LC_ALL="${language}"
