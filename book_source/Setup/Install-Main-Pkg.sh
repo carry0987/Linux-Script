d1791f64096717ad81b123ffda44d3e571dc7a3c
carry0987 <carry0987@users.noreply.github.com>
2024-02-27 20:00:07 +0800

    Update Install-Main-Pkg.sh

--- book_source/Setup/Install-Main-Pkg.sh
+++ book_source/Setup/Install-Main-Pkg.sh
@@ -23,6 +23,20 @@ install_guest_agent() {
     systemctl start qemu-guest-agent
 }
 
+# Check if the custom sudoers file exists, and create/overwrite it
+create_nopasswd_sudoers() {
+    echo "Checking for no-password sudo configuration..."
+    if [ ! -f /etc/sudoers.d/nopasswd ]; then
+        echo "Creating no-password sudo configuration for the sudo group."
+        echo "%sudo	ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nopasswd > /dev/null
+        # Ensure the file has the correct permissions
+        execute chmod 0440 /etc/sudoers.d/nopasswd
+        echo "The sudo group can now use sudo without a password."
+    else
+        echo "No-password sudo configuration already exists."
+    fi
+}
+
 execute apt-get update
 execute apt-get -y dist-upgrade
 execute apt-get -y install ssh zip unzip wget vim screen haveged htop ufw
@@ -38,6 +52,8 @@ execute systemctl enable haveged
 execute systemctl start haveged
 # Change default editor to vim
 execute update-alternatives --set editor /usr/bin/vim.basic
+# Configure sudo to not require password for current user
+create_nopasswd_sudoers
 
 echo 'Finish !'
 exit 0
